f7555f8d473ad8d6b65de4eaa4c071cd4d0b5245
Dmitry <dmitrygreshnov2@gmail.com>
2025-07-28 12:28:56 +0300

    initial commit

---
+++ src/redux/slice/searchParamsSlice.ts
@@ -0,0 +1,36 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const savedData = localStorage.getItem("search");
+
+const emptyState = {
+  from_city_id: "", //Идентификатор города, откуда планируется путешествие //string
+  to_city_id: "", //Идентификатор города, куда планируется путешествие //string
+  date_start: "", //Дата отбытия туда (в формате YYYY-MM-DD; например 2030-03-01) //string
+  date_end: "", //Дата отбытия обратно (в формате YYYY-MM-DD; например 2030-03-01) //string
+};
+
+const initialState = savedData ? JSON.parse(savedData) : emptyState;
+
+const searchParamsSlice = createSlice({
+  name: "searchParamsSlice",
+  initialState,
+  reducers: {
+    addOrChangeSearchParameter(state, action) {
+      const { name, value } = action.payload;
+      state[name] = value;
+    },
+    swapCityId(state) {
+      [state.from_city_id, state.to_city_id] = [
+        state.to_city_id,
+        state.from_city_id,
+      ];
+    },
+    removeSearchParams() {
+      return emptyState;
+    },
+  },
+});
+
+export const { addOrChangeSearchParameter, swapCityId, removeSearchParams } = searchParamsSlice.actions;
+
+export default searchParamsSlice;
